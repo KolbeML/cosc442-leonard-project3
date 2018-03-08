cb660aeae1282ef222dbaed491e0355a6bbcbc77
KolbeML <kleona3@students.towson.edu>
2018-03-07 19:40:27 -0500

    Final commit - 100% coverage for VendingMachine2

---
+++ VendingMachine2/src/tests/TestAll.java
@@ -0,0 +1,34 @@
+package tests;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * The class <code>TestAll</code> builds a suite that can be used to run all
+ * of the tests within its package as well as within any subpackages of its
+ * package.
+ *
+ * @generatedBy CodePro at 3/5/18, 12:27 PM
+ * @author Kolbe
+ * @version $Revision: 1.0 $
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	VendingMachineTest.class,
+	VendingMachineItemTest.class,
+	VendingMachineExceptionTest.class,
+})
+public class TestAll {
+
+	/**
+	 * Launch the test.
+	 *
+	 * @param args the command line arguments
+	 *
+	 * @generatedBy CodePro at 3/5/18, 12:27 PM
+	 */
+	public static void main(String[] args) {
+		JUnitCore.runClasses(new Class[] { TestAll.class });
+	}
+}
